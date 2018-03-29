a2cd987b59006ee27ccfcfa6635a5c151be60df5
Giulia Mueller <giulia_mueller@yahoo.com>
2018-03-29 10:17:41 -0400

    added Menu Items to MenuController.cs

--- src/CheeseMVC/Controllers/MenuController.cs
+++ src/CheeseMVC/Controllers/MenuController.cs
@@ -2,6 +2,7 @@
 using CheeseMVC.Models;
 using CheeseMVC.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -49,6 +50,31 @@ namespace CheeseMVC.Controllers
             return View(addMenuViewModel);
         }
 
+        public IActionResult ViewMenu(int id)
+        {
+            if (id == 0)
+            {
+                return Redirect("/Menu");
+            }
+
+            Menu theMenu = context.Menus.Single(m => m.ID == id);
+
+            List<CheeseMenu> items = context
+                .CheeseMenus
+                .Include(item => item.Cheese)
+                .Where(cm => cm.MenuID == id)
+                .ToList();
+            ViewMenuViewModel viewMenuViewModel = new ViewMenuViewModel { Menu = theMenu, Items = items };
+
+            ViewBag.title = theMenu.Name;
+            return View(viewMenuViewModel);
+
+        }
+        public IActionResult AddItem(int id)
+        {
+            Menu menu = context.Menus.Single(m => m.ID == id);
+            return View();
+        }
 
     }
 }
